cc5ac5e17f2bd15aae348dd3f3d8c35ed99ad422
DivyaAvtaran212 <109351482+DivyaAvtaran212@users.noreply.github.com>
2026-06-25 23:30:54 +0530

    Two sum problem

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -2,11 +2,18 @@ package arrays;
 
 public class TwoSum {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("DSA Repository Started");
-	}
-	
-	
+	public int[] twoSum(int[] nums, int target) {
+        int[] arr = new int[2];
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+               if(nums[i]+nums[j]==target){
+                arr[0]=i;
+                arr[1]=j;
+               } 
+            }
+        }
+        return arr;
+    }
 
 }
+
